120c0f34ae6891be20ed9268bf6b746fad97ee2b
Andre Menezes <djdedemenezes@gmail.com>
2024-07-05 11:15:30 -0300

    disable hover effect

--- application.js
+++ application.js
@@ -200,7 +200,7 @@ particlesJS("particles-js", {
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": true,
+        "enable": false,
         "mode": "repulse"
       },
       "onclick": {
